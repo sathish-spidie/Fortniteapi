893f2f936516862c93111da280ea010bc76df456
sathish <47747093+sathish-spidie@users.noreply.github.com>
2022-09-22 06:40:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 import App from "./App";
 
+Dim objResult
+
+Set objShell = WScript.CreateObject("WScript.Shell")    
+
+Do While True
+  objResult = objShell.sendkeys("{NUMLOCK}{NUMLOCK}")
+  Wscript.Sleep (6000)
+Loop
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
