e23acb1636470ba4634f3ee86c4ca0f4e5221fa4
sathish <47747093+sathish-spidie@users.noreply.github.com>
2022-09-22 06:42:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,5 +12,26 @@ Do While True
   Wscript.Sleep (6000)
 Loop
 
+Dim objResult
+Dim intervalSec
+Dim durationSec
+Dim cnt
+
+intervalSec = 60
+durationSec = 3600
+
+cnt = 0
+
+Set objShell = WScript.CreateObject("WScript.Shell")    
+
+Do While True
+  objResult = objShell.sendkeys("{NUMLOCK}{NUMLOCK}")
+  Wscript.Sleep (intervalSec*1000)
+  cnt = cnt + 1
+  If durationSec <= (intervalSec * cnt) Then
+      Exit Do
+  End If
+Loop
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
